15186498556ce6bd916c037adf41a5a3d075fc4b
dezldog <dezldog@gmail.com>
2016-02-09 20:15:31 -0800

    We'll see. It _is_ skipping a second here and there on the display

--- gps_lcd_moving_due/gps_lcd_moving_due.ino
+++ gps_lcd_moving_due/gps_lcd_moving_due.ino
@@ -1,113 +1,67 @@
 //dezldog
-// Playing with my gps and LCD
+// Playing with my GPS and LCD
 // some code mine, some code borrowed from Adafruit.
 // Buy from them; they support the community. Alibaba vendors, etc, don't
 //
 
-#include "SoftwareSerial.h"
-#include "Adafruit_GPS.h"
+#include "Wire.h"
 #include "Adafruit_LiquidCrystal.h"
-
-// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
-// Set to 'true' if you want to debug and listen to the raw GPS sentences.
-#define GPSECHO  false
+#include "Adafruit_GPS.h"
 
 // Metric or not? Synthetica! metric = true, imperial = false
 boolean displayUnits = true;
-
-// create LCD object
-Adafruit_LiquidCrystal lcd(0);
-
-//Set up softwareserial
-int rxPin = 8;
-int txPin = 7;
-int GPSBaud = 9600;
-
-// Variables for formatting
 int hours = 0;
 int minutes = 0;
 int seconds = 0;
 int sats = 0;
 float velocity = 0;
+uint32_t timer = millis();
 
-// create gps sensor connection object
-SoftwareSerial gpsSerial(rxPin, txPin);
-
-// create gps object
-Adafruit_GPS GPS(&gpsSerial);
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences.
+#define GPSECHO  false
 
 // this keeps track of whether we're using the interrupt
 // off by default!
 boolean usingInterrupt = false;
 void useInterrupt(boolean); // Func prototype keeps Arduino 0023 happy
-uint32_t timer = millis();
+
+// create LCD object
+Adafruit_LiquidCrystal lcd(0);
+
+//define serial port and use it with GPS
+// GPS power pin to Arduino Due 3.3V output.
+// GPS ground pin to Arduino Due ground.
+// For hardware serial 1 (recommended):
+//   GPS TX to Arduino Due Serial1 RX pin 19
+//   GPS RX to Arduino Due Serial1 TX pin 18
+#define gpsSerial Serial1
+
+Adafruit_GPS GPS(&gpsSerial);
+
 
 void setup()
 {
-  //start gps
-  Serial.begin(57600);
-  gpsSerial.begin (GPSBaud);
+  //start GPS
+  Serial.begin(115200);
+  GPS.begin(9600);
+  gpsSerial.begin(9600);
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_5HZ);
-  GPS.sendCommand(PGCMD_ANTENNA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
 
   // the nice thing about this code is you can have a timer0 interrupt go off
   // every 1 millisecond, and read data from the GPS for you. that makes the
   // loop code a heck of a lot easier!
+
+#ifdef __arm__
+  usingInterrupt = false;  //NOTE - we don't want to use interrupts on the Due
+#else
   useInterrupt(true);
+#endif
 
-  // Print a message to the LCD
-  lcd.begin(16, 4);
-  lcd.print("Welcome to Dezldog");
-  lcd.setCursor(0, 1);
-  lcd.print("Start-Up Complete");
-  lcd.setCursor(0, 2);
-  lcd.print(PMTK_Q_RELEASE);
-  lcd.setCursor(0, 3);
-  for (int x = 0; x < 20; x++)
-  {
-    lcd.setCursor(x, 3);
-    lcd.print("0");
-    delay(200);
-    lcd.setCursor(x - 1, 3);
-    lcd.print(" ");
-  }
-  lcd.clear();
-}
+delay(1000);
 
-void loop()
-{
-  // in case you are not using the interrupt above, you'll
-  // need to 'hand query' the GPS, not suggested :(
-  if (! usingInterrupt)
-  {
-    // read data from the GPS in the 'main loop'
-    char c = GPS.read();
-    // if you want to debug, this is a good time to do it!
-    if (GPSECHO)
-      if (c) Serial.print(c);
-  }
-
-  if (GPS.newNMEAreceived())
-  {
-    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
-      return;  // we can fail to parse a sentence in which case we should just wait for another
-  }
-
-  // if millis() or timer wraps around, we'll just reset it
-  if (timer > millis())  timer = millis();
-
-  // approximately every 1 seconds or so, print out the current stats
-  if (millis() - timer > 950)
-  {
-    timer = millis(); // reset the timer
-  }
-
-  displayLcd();
-//  writeToSerial();
-
-}
-
+#ifdef __AVR__
 // Interrupt is called once a millisecond, looks for any new GPS data, and stores it
 SIGNAL(TIMER0_COMPA_vect) {
   char c = GPS.read();
@@ -132,6 +86,61 @@ void useInterrupt(boolean v) {
     TIMSK0 &= ~_BV(OCIE0A);
     usingInterrupt = false;
   }
+}
+#endif //#ifdef__AVR__
+
+// Print a message to the LCD
+lcd.begin(16, 4);
+lcd.print("Start-Up Complete");
+lcd.setCursor(0, 1);
+lcd.print("Welcome to Dezldog");
+lcd.setCursor(0, 2);
+lcd.print(PMTK_Q_RELEASE);
+lcd.setCursor(0, 3);
+for (int x = 0; x < 20; x++)
+{
+  lcd.setCursor(x, 3);
+  lcd.print("0");
+  delay(200);
+  lcd.setCursor(x - 1, 3);
+  lcd.print(" ");
+}
+lcd.clear();
+}
+
+void loop()
+{
+  // in case you are not using the interrupt above, you'll
+  // need to 'hand query' the GPS, not suggested :(
+  if (! usingInterrupt) {
+    // read data from the GPS in the 'main loop'
+    char c = GPS.read();
+    // if you want to debug, this is a good time to do it!
+    if (GPSECHO)
+      if (c) Serial.print(c);
+  }
+  
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences! 
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+  
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis())  timer = millis();
+
+  // approximately every 2 seconds or so, print out the current stats
+  if (millis() - timer > 990) { 
+    timer = millis(); // reset the timer
+
+//  writeToSerial();
+  displayLcd();
+}
 }
 
 void displayLcd()
@@ -216,9 +225,9 @@ void displayLcd()
   lcd.setCursor(0, 3);
   lcd.print("Lon:"); lcd.print(GPS.longitudeDegrees, 6);
 }
-/*
-  void writeToSerial()
-  {
+
+void writeToSerial()
+{
   Serial.print("\nTime: ");
   Serial.print(GPS.hour, DEC); Serial.print(':');
   Serial.print(GPS.minute, DEC); Serial.print(':');
@@ -243,5 +252,5 @@ void displayLcd()
     Serial.print("Altitude: "); Serial.println(GPS.altitude);
     Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
   }
-  }
-*/
+}
+
